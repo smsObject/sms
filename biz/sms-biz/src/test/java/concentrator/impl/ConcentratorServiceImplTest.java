7fc76000dc579b043d5dba8cac85ce33e21a4941
胡弼华 <hubihua@qccr.com>
2016-10-06 17:26:05 +0800

    修改

---
+++ biz/sms-biz/src/test/java/concentrator/impl/ConcentratorServiceImplTest.java
@@ -0,0 +1,96 @@
+package concentrator.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* ConcentratorServiceImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ʮ�� 6, 2016</pre> 
+* @version 1.0 
+*/
+
+public class ConcentratorServiceImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: add(Concentrator concentrator) 
+* 
+*/ 
+@Test
+public void testAdd() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: list(Concentrator concentrator) 
+* 
+*/ 
+@Test
+public void testList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getConcentratorById(Integer id) 
+* 
+*/ 
+@Test
+public void testGetConcentratorById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: update(Concentrator concentrator) 
+* 
+*/ 
+@Test
+public void testUpdate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: delete(Integer id) 
+* 
+*/ 
+@Test
+public void testDelete() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: page(Concentrator concentrator) 
+* 
+*/ 
+@Test
+public void testPage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateByComPort(Concentrator concentrator) 
+* 
+*/ 
+@Test
+public void testUpdateByComPort() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
