f08e178ed99c4843d9894debbb9d3a417ee413ab
witwo <627008031@qq.com>
2017-05-13 18:18:54 +0800

    修改

--- biz/sms-biz/src/main/java/com/hbh/sms/biz/service/system/impl/SystemRoleServiceImpl.java
+++ biz/sms-biz/src/main/java/com/hbh/sms/biz/service/system/impl/SystemRoleServiceImpl.java
@@ -1,38 +1,62 @@
 package com.hbh.sms.biz.service.system.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.hbh.sms.biz.service.system.SystemRoleService;
+import com.hbh.sms.dal.dao.SystemRoleMapper;
 import com.hbh.sms.model.entity.SystemRole;
 import com.sms.common.PagedData;
 import com.sms.common.Result;
+import com.sms.common.ResultUtil;
 import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by hbh
  */
 @Service("systemRoleService")
 public class SystemRoleServiceImpl implements SystemRoleService {
+    @Resource
+    private SystemRoleMapper systemRoleMapper;
+
     @Override
     public Result<Long> add(SystemRole systemRole) {
-        return null;
+        systemRoleMapper.insert(systemRole);
+        return ResultUtil.newSuccessResult(systemRole.getId());
     }
 
     @Override
     public Result<Boolean> deleteById(Long id) {
-        return null;
+        int i = systemRoleMapper.deleteByPrimaryKey(id);
+        return ResultUtil.newSuccessResult(i>0);
     }
 
     @Override
     public Result<Boolean> update(SystemRole systemRole) {
-        return null;
+        int i = systemRoleMapper.updateByPrimaryKey(systemRole);
+        return ResultUtil.newSuccessResult(i>0);
     }
 
     @Override
     public Result<PagedData<SystemRole>> page(SystemRole systemRole) {
-        return null;
+        Result<PagedData<SystemRole>> result = null;
+        Page page = PageHelper.startPage(systemRole.getPageNo() , systemRole.getPageSize());
+        List<SystemRole> list = systemRoleMapper.query(systemRole);
+        PagedData<SystemRole> pagedData = new PagedData<>();
+        pagedData.setPageNo(systemRole.getPageNo());
+        pagedData.setPageSize(systemRole.getPageSize());
+        pagedData.setResultList(list);
+        pagedData.setTotalSize(page.getTotal());
+        return ResultUtil.newSuccessResult(pagedData);
     }
 
     @Override
     public Result<SystemRole> getUserInfoById(Long id) {
-        return null;
+        SystemRole systemRole = new SystemRole();
+        systemRole.setId(id);
+        List<SystemRole> list = systemRoleMapper.query(systemRole);
+        systemRole = list.isEmpty()?null:list.get(0);
+        return ResultUtil.newSuccessResult(systemRole);
     }
 }
--- biz/sms-biz/src/main/java/com/hbh/sms/biz/service/system/impl/SystemUserServiceImpl.java
+++ biz/sms-biz/src/main/java/com/hbh/sms/biz/service/system/impl/SystemUserServiceImpl.java
@@ -1,38 +1,63 @@
 package com.hbh.sms.biz.service.system.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.hbh.sms.biz.service.system.SystemUserService;
+import com.hbh.sms.dal.dao.SystemUserMapper;
 import com.hbh.sms.model.entity.SystemUser;
 import com.sms.common.PagedData;
 import com.sms.common.Result;
+import com.sms.common.ResultUtil;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by hbh
  */
 @Service("systemUserService")
 public class SystemUserServiceImpl implements SystemUserService {
+    @Resource
+    private SystemUserMapper systemUserMapper;
+
     @Override
     public Result<Long> add(SystemUser systemUser) {
-        return null;
+        systemUserMapper.insert(systemUser);
+        return ResultUtil.newSuccessResult(systemUser.getId());
     }
 
     @Override
     public Result<Boolean> deleteById(Long id) {
-        return null;
+        int i = systemUserMapper.deleteByPrimaryKey(id);
+        return ResultUtil.newSuccessResult(i>0);
     }
 
     @Override
     public Result<Boolean> update(SystemUser systemUser) {
-        return null;
+        int i = systemUserMapper.updateByPrimaryKey(systemUser);
+        return ResultUtil.newSuccessResult(i>0);
     }
 
     @Override
     public Result<PagedData<SystemUser>> page(SystemUser systemUser) {
-        return null;
+        Result<PagedData<SystemUser>> result = null;
+        Page page = PageHelper.startPage(systemUser.getPageNo() , systemUser.getPageSize());
+        List<SystemUser> list = systemUserMapper.query(systemUser);
+        PagedData<SystemUser> pagedData = new PagedData<>();
+        pagedData.setPageNo(systemUser.getPageNo());
+        pagedData.setPageSize(systemUser.getPageSize());
+        pagedData.setTotalSize(page.getTotal());
+        pagedData.setResultList(list);
+        return ResultUtil.newSuccessResult(pagedData);
     }
 
     @Override
     public Result<SystemUser> getUserInfoById(Long id) {
-        return null;
+        SystemUser systemUser = new SystemUser();
+        systemUser.setId(id);
+        List<SystemUser> list = systemUserMapper.query(systemUser);
+        systemUser = list.isEmpty()?null:list.get(0);
+        return ResultUtil.newSuccessResult(systemUser);
     }
 }
